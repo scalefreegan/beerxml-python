8c22e3e019f9fedb398d380d4dc068c4a917e81e
Aaron Brooks <aaron.neil.brooks@gmail.com>
2024-10-15 09:59:18 -0600

    add message to confirm custom version

--- beerxml/__init__.py
+++ beerxml/__init__.py
@@ -1,2 +1,6 @@
 from .models import *  # noqa: F403
 from .beerxml_parser import BeerxmlParser  # noqa: F401
+
+__version__ = "0.1.0_anb"
+# print custom version on package import
+print(f"BeerXML Python version: {__version__}")
